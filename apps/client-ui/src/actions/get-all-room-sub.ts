617927bef6d74aa85d9dda3f7c96c9a43fe34f31
Đỗ Nhật Nam <119683629+DoNhatNam1@users.noreply.github.com>
2024-03-12 14:33:32 +0700

    first commit

---
+++ apps/client-ui/src/actions/get-all-room-sub.ts
@@ -0,0 +1,26 @@
+"use server";  // Sử dụng server mode
+
+import prisma from "../lib/prismaDb";  // Import module prisma từ đường dẫn ../lib/prismaDb
+import { cookies } from 'next/headers';  // Import cookies từ 'next/headers'
+
+// Hàm lấy thông tin tất cả các phòng con
+export default async function getAllRoomSubByUserId()  {
+    const admin_id = cookies().get('user_id')?.value  // Lấy user_id từ cookies
+    const role = cookies().get('role')?.value  // Lấy user_id từ cookies
+    const khach_san = cookies().get('khach_san')?.value
+    if(role === 'Admin'){
+        const RoomSubQuery = await prisma.tbNhomKhuVucPhong.findMany({  // Tìm tất cả các phòng con từ bảng tbNhomKhuVucPhong
+            where: {
+                MaKhachSan: khach_san!
+            },
+            select: {
+                id: true,
+                TenNhomKhuVuc: true,
+                GhiChu: true,
+            }
+        })
+        return RoomSubQuery;  // Trả về danh sách các phòng con
+    }
+
+
+}
